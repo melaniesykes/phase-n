593d206d28aaa0f66deff02705f57eaefaea7605
Melanie <melaniecorin1@gmail.com>
2024-06-05 16:08:12 -0500

    match server repo

--- docs/main.js
+++ docs/main.js
@@ -3,15 +3,6 @@ let urlParams = new URLSearchParams(window.location.search);
 let game = urlParams.get("game");
 let id = urlParams.get("id");
 
-// Colors are implemented by hue-rotating a red card via CSS.
-let colors = {
-    "B":200,
-    "G":150,
-    "R":0,
-    "P":290
-    // "P":167
-    // "P":60
-}
 
 // Get player name + turn number
 let player_name, player_turn;
@@ -42,10 +33,11 @@ function getData(){
                 console.log("error: game not found");
             }
             else{
-                console.log(JSON.stringify(data));
+                // console.log(JSON.stringify(data));
                 turn = data["turn"]; 
                 setCards(data["player_cards"]);
-                setStandings(data["num_cards"]);
+                setStandings(data['public_info']);
+                setPhasePiles(data["player_piles"], data["completed"]);
                 setTopcard(data["top_card"]);
                 setPlayerTurnDisplay();
             }
@@ -53,80 +45,108 @@ function getData(){
     )
 }
 
-function getCardHTML(card){
-    // Returns HTML of given card
-    // card (str): card to display
-    // clickable (bool): whether card is playable (adds onclick part, and influences)
-    // the image of the wildcard
-    let parts = card.split("_");
-    let hue = colors[parts[0]];
-    let symbol = parts[1];
-    
-    let onclick = `onmousedown="playCard('${card}')"`;
-
-    let img_src = "card.png";
-
-    if (symbol == "CANCEL"){
-        symbol = "";
-        img_src = "cancel.png";
-    }
-
-    if (symbol == "WILD"){
-        return `<div class=card ${onclick}><img src=assets/wild.png></div>`;
-    }
-    return `<div class=card style="filter:hue-rotate(${hue}deg);" ${onclick}><img src=assets/${img_src}><p>${symbol}</p></div>`;
-}
 
 function setCards(cards){
     // Clear currently displayed cards
     $("#card-grid").html("");
     // Add each card
-    for (let card of cards){
+    for (let [card_id, card_file] of Object.entries(JSON.parse(cards))) {
+        let onclick = `onmousedown="selectCard('${card_id}')"`
+        let card = `<div class=hand-card ${onclick}><img src=assets/${card_file}></div>`
         $("#card-grid").append(card);
-        // $("#card-grid").append(getCardHTML(card));
     }
+}
+
+function setPhasePiles(piles, completed){
+    // Display standings (how many cards each player has)
+    $("#phase-grid").html("");
+    for (let [pile_name, pile_cards] of Object.entries(JSON.parse(piles))){
+        // console.log(completed.includes(pile_name), 'pile name', pile_name, 'cards', pile_cards, 'completed', completed)
+
+        if (completed.includes(pile_name)) {
+            color = "#66b85f";
+        } else {
+            color = "#f9a538";
+        }
+
+        let div_html = `<div class=phase-pile style="background-color:${color}">`;
+        console.log(div_html)
+
+        // let div_html = "<div class=phase-pile>";
+        div_html += `<h2 class=pile-name onmousedown="selectPile(null, '${pile_name}')">${pile_name}</h2>`;
+        div_html += "<div class=pile-cards-div>";
+        
+        for (const [card_id, card_file] of Object.entries(pile_cards)) {
+            // console.log('i', card_id, 'card', card_file)
+
+            let onclick = `onmousedown="selectCard('${card_id}')"`;
+
+            let card = `<div class=pile-card-div ${onclick}><img src=assets/${card_file} class=pile-card></div>`;
+            div_html += card;
+        }                
+        div_html += '</div></div>';
+        $("#phase-grid").append(div_html);
+    }
+}
+
+function labeledPile(pile_name, card_list, player_id) {
+    let div_html = `<div class=phase-pile  onmousedown="selectPile('${player_id}', '${pile_name}')">`;
+    if (pile_name) {
+        div_html += `<h5 class=pile-name>${pile_name}</h5>`;
+    }
+    div_html += "<div class=pile-cards-div>";
+    // console.log('pile_name', card_id, 'card', card_file)
+    for (card_file of card_list) {
+        let card = `<div class=pile-card-div ><img src=assets/${card_file} class=pile-card></div>`;
+        div_html += card;
+    }
+    div_html += "</div></div>";
+    return div_html
 }
 
 function setStandings(standings){
     // Display standings (how many cards each player has)
     $("#standings").html("");
-    for (let i=0; i<standings.length; i++){
-        player = standings[i];
-        // if (i == turn) $("#standings").append(`<tr style="background-color:#f7913e;"><td><b>${player[0]}</b></td><td><b>${player[1]}</b></td>`);
-        // else $("#standings").append(`<tr><td>${player[0]}</td><td>${player[1]}</td>`);
+
+    for (let [player_num, player_info] of Object.entries(JSON.parse(standings))) {
 
         let div_html = "<div class=player-info>";
-        if (i == turn){
+        if (player_num == turn){
             div_html = '<div class=player-info style="background-color:#f7913e;border:4px solid black">';
         }
-        div_html += `<h2>${player[0]}</h2>`;
-
-        div_html += "<br><div class=marker-card-div>";
-        for (let i=0; i<player[1]; i++){
-            if (i==0){ // This is necessary to make sure the player-info div has the right height
-                div_html += `<img src=assets/hiddencard.png class=marker-card style='position:relative'>`;
+        div_html += `<h2 onmousedown="selectPlayer('${player_info['id']}')">${player_info['name']} ${player_info['n_cards']}</h2>`;
+        
+        div_html += "<div class=pile-grid>"
+        for (let [pile_name, pile_cards] of Object.entries(player_info['piles'])){
+            if (pile_name != 'hand') {
+                div_html += labeledPile(pile_name, pile_cards, player_info['id']);
             }
-            div_html += `<img src=assets/hiddencard.png class=marker-card style='left:${i*8}%'>`;
         }
-        div_html += "</div></div>";
+        div_html += "</div>";
+
+        div_html += "<div class=pile-grid>"
+        div_html += labeledPile('', player_info['piles']['hand']);
+        div_html += "</div>";
+
+        div_html += "</div>";
         $("#standings").append(div_html);
 
         // Check if someone has won and display winner message
-        if (player[1] == 0){
+        if (player_info['n_cards'] == 0){
             $("#win-display").css("display","block");
-            $("#winner-name").html(`${player[0]} won the game!`);
+            $("#winner-name").html(`${player_info['name']} won the game!`);
             // Stop the update interval
             clearInterval(dataInterval);
             game_over = true;
-            setInterval(checkIfReturn,1000);
+            // setInterval(checkIfReturn,1000);
         }
     }
 }
 
+
 function setTopcard(card){
     // Display current top card
     document.getElementById("top-card").innerHTML = card;
-    // document.getElementById("top-card").innerHTML = getCardHTML(card);
 }
 
 function setPlayerTurnDisplay(){
@@ -147,37 +167,60 @@ getData();
 
 // Send move requests
 
-function playCard(card){
+function selectPile(pile_owner_id, pile_name){
     if (game_over){
         return "game over";
     }
+    // Send request to move card
+    $.post(SERVERURL+"selectPile",JSON.stringify({
+        game:game,
+        id:id,
+        pile:pile_name,
+        pile_owner_id:pile_owner_id
 
-    // Check if turn is correct
-    if (turn != player_turn){
-        return "not player turn";
+    }),getData);
+}
+
+function selectCard(card){
+    if (game_over){
+        return "game over";
     }
-
-    // This function may be called by the color-selection section,
-    // which means we need to hide it on click
-    $("#color-selection").css("display", "none");
-
-    // Send request to play card
-    $.post(SERVERURL+"play",JSON.stringify({
+    // Send request to move card
+    $.post(SERVERURL+"selectCard",JSON.stringify({
         game:game,
         id:id,
         card:card
     }),getData);
 }
 
-function drawCard(){
+function selectDeck(){
     if (game_over){
         return "game over";
     }
-
-    // Send request to draw card to server
-    $.post(SERVERURL+"draw",JSON.stringify({
+    // Send request to move card
+    $.post(SERVERURL+"selectDeck",JSON.stringify({
         game:game,
-        id:id
+        id:id,
+    }),getData);
+}
+
+function selectDiscard(){
+    if (game_over){
+        return "game over";
+    }
+    // Send request to move card
+    $.post(SERVERURL+"selectDiscard",JSON.stringify({
+        game:game,
+        id:id,
+    }),getData);
+}
+
+function selectPlayer(selected_player){
+    // Send request to move card
+    $.post(SERVERURL+"selectPlayer",JSON.stringify({
+        game:game,
+        id:id,
+        selected_player:selected_player
     }),getData);
 }
 
