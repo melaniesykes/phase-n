45ebd44410add125fcb65dcf074fe147ec3eb295
Melanie <melaniecorin1@gmail.com>
2024-06-06 19:03:27 -0500

    use server

--- docs/constants.js
+++ docs/constants.js
@@ -1,2 +1,2 @@
-SERVERURL = "http://127.0.0.1:5000/";
-// SERVERURL = "https://phasen.pythonanywhere.com/";
+// SERVERURL = "http://127.0.0.1:5000/";
+SERVERURL = "https://phasen.pythonanywhere.com/";
